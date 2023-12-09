2ddeac512e74b65d4b7cdc8c89ba0be732a2f1bb
cristian reyes <cristianreyes6777@gmail.com>
2023-12-09 16:52:49 -0500

    [rptected seeds

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,12 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+require('dotenv').config(); // Load environment variables from .env file
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql', 
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+});
 
 module.exports = sequelize;
